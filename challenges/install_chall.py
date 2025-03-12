eaf23d706d6028bc2fa27522cf37d165cab05c91
Khalil-420 <khalyylgam@gmail.com>
2025-03-12 10:44:21 -0400

    adding challs

--- challenges/install_chall.py
+++ challenges/install_chall.py
@@ -1,6 +1,6 @@
 import os,glob
 
-categories = ["web","forensics","misc","pwn","hardware","osint","crypto"]
+categories = ["web","forensics","misc","osint","crypto","pwn"]
 
 for category in categories:
     if os.path.exists(f"./{category}"):
@@ -9,7 +9,7 @@ for category in categories:
         challenges.sort()
         for challenge in challenges:
                 print(f"[+] Installing challenge: {challenge}")
-                os.system(f"python3 -m ctfcli challenge add \"{challenge}\" && python3 -m ctfcli challenge install \"{challenge}\"")    
+                os.system(f"python3 -m ctfcli challenge add \"{challenge}\" && python3 -m ctfcli challenge install \"{challenge}\"")
     else:
         print(f"[-] Cant find : {category}")
         exit(1)
